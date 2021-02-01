4286dacd025647581baa7947cd48be0292eaa3f3
Kostiantyn Kolchenko <kolchkos@fit.cvut.cz>
2021-02-01 11:11:43 +0100

    + BookDto

---
+++ src/main/java/cz/cvut/fit/tjv/semestral/rest_api/model/BookDto.java
@@ -0,0 +1,113 @@
+package cz.cvut.fit.tjv.semestral.rest_api.model;
+
+import org.springframework.hateoas.RepresentationModel;
+import java.util.Collection;
+
+public class BookDto extends RepresentationModel<BookDto> {
+    private Long id;
+    private String bookName;
+    private Integer satisfactionScore;
+    private String publishDate;
+    private Integer issueNumber;
+    private String description;
+    private Collection<Long> creatorId;
+
+    public BookDto() {
+    }
+
+    public BookDto( Long id, String bookName, Integer satisfactionScore, String publishDate,
+                    Integer issueNumber, String description, Collection<Long> creatorId) {
+        this.id = id;
+        this.bookName = bookName;
+        if ( satisfactionScore == null )
+            this.satisfactionScore = 0;
+        else
+            this.satisfactionScore = satisfactionScore;
+        this.publishDate = publishDate;
+        this.issueNumber = issueNumber;
+        this.description = description;
+        this.creatorId = creatorId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id){ this.id = id; }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Integer getSatisfactionScore() {
+        return satisfactionScore;
+    }
+
+    public void setSatisfactionScore(Integer satisfactionScore) {
+        this.satisfactionScore = satisfactionScore;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public Integer getIssueNumber() {
+        return issueNumber;
+    }
+
+    public void setIssueNumber(Integer issueNumber) {
+        this.issueNumber = issueNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Collection<Long> getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Collection<Long> creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BookDto bookDto = (BookDto) o;
+
+        if (!id.equals(bookDto.id)) return false;
+        if (!bookName.equals(bookDto.bookName)) return false;
+        if (!satisfactionScore.equals(bookDto.satisfactionScore)) return false;
+        if (publishDate != null ? !publishDate.equals(bookDto.publishDate) : bookDto.publishDate != null) return false;
+        if (issueNumber != null ? !issueNumber.equals(bookDto.issueNumber) : bookDto.issueNumber != null) return false;
+        if (description != null ? !description.equals(bookDto.description) : bookDto.description != null) return false;
+        return creatorId != null ? creatorId.equals(bookDto.creatorId) : bookDto.creatorId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + bookName.hashCode();
+        result = 31 * result + satisfactionScore.hashCode();
+        result = 31 * result + (publishDate != null ? publishDate.hashCode() : 0);
+        result = 31 * result + (issueNumber != null ? issueNumber.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (creatorId != null ? creatorId.hashCode() : 0);
+        return result;
+    }
+}
